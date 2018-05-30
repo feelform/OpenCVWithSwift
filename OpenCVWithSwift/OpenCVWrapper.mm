6cd7e8d5c375969a04b39e823adbbd7f6e032dbc
yongseok kang <yongseok.kang@navercorp.com>
2018-05-30 17:13:29 +0900

    Add cvMat from UIImage

--- OpenCVWithSwift/OpenCVWrapper.mm
+++ OpenCVWithSwift/OpenCVWrapper.mm
@@ -8,6 +8,7 @@
 
 #import "OpenCVWrapper.h"
 #import <opencv2/opencv.hpp>
+#import <UIKit/UIKit.h>
 
 @implementation OpenCVWrapper
 
@@ -15,4 +16,35 @@
     return [NSString stringWithFormat:@"OpenCV Version %s",  CV_VERSION];
 }
 
++ (instancetype)sharedInstance {
+    static id sharedInstance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+
+- (cv::Mat)cvMatFromUIImage:(UIImage *)image {
+    CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
+    CGFloat cols = image.size.width;
+    CGFloat rows = image.size.height;
+    
+    cv::Mat cvMat(rows, cols, CV_8UC4); // 8 bits per component, 4 channels (color channels + alpha)
+    
+    CGContextRef contextRef = CGBitmapContextCreate(cvMat.data,                 // Pointer to  data
+                                                    cols,                       // Width of bitmap
+                                                    rows,                       // Height of bitmap
+                                                    8,                          // Bits per component
+                                                    cvMat.step[0],              // Bytes per row
+                                                    colorSpace,                 // Colorspace
+                                                    kCGImageAlphaNoneSkipLast |
+                                                    kCGBitmapByteOrderDefault); // Bitmap info flags
+    
+    
+    CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
+    CGContextRelease(contextRef);
+    
+    return cvMat;
+}
 @end
